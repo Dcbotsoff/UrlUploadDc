58d065458f9f1602ae24d8023cec4c674f3f1d77
Selfie SD <webtamil0@gmail.com>
2022-02-05 16:46:34 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,8 @@
 class Translation(object):
     START_TEXT = """Hi {},I'm URL Uploader! Click /help for more details!
 
+I am Also support Utube ✅😂
+
 @Groupdcbots"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
